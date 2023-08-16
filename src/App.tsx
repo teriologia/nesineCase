20108ff47fde7617573b3dccc345301747934c89
mserdar.tekin <Mserdar.tekin@gvg.com.tr>
2023-08-16 14:58:52 +0300

    basic react implements and scripts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+ 
+  return (
+    <div className="mainContainer">
+     <p>Testing</p>
+    </div>
+  );
+}
+
+export default App;
